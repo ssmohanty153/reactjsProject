e1ab1172d3bf9998c2c2b0ffa1d47739083a519f
ssmohanty153 <ssmohanty153@gmail.com>
2022-02-26 21:30:18 +0530

    how to design function component in react

---
+++ src/functioncomp.js
@@ -0,0 +1,43 @@
+const HeaderComonent=()=>(
+    <>
+    <div>
+        <h3 className="bg-primary text-white text-center">Amazone shoping</h3>
+    </div>
+    </>
+)
+const NagationComponent=()=>(
+    <>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    </>
+)
+
+const FootComponent=()=>(
+    <>
+    <div className="bg-dark text-center text-white">&copy: copyright 2021
+    </div> 
+    </>
+)
+
+const MainComponnt=()=>(
+    <>
+    <HeaderComonent/>
+    <div className="row" style={{height:"550px"}}>
+        <div className="col-5">
+            <NagationComponent/>
+
+        </div>
+        <div className="col-7">
+            <h4>OnlineShping</h4>
+
+        </div>
+    </div>
+    <div>
+        <FootComponent/>
+    </div>
+    </>
+)
+ReactDOM.render(<MainComponnt/>,document.getElementById("container"));
